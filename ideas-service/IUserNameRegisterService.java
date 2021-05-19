e1f2b2b7f6f2b6bd50d6780b32514e74667e8ff2
ClearLovePlus <33109146+ClearLovePlus@users.noreply.github.com>
2021-05-19 16:33:01 +0800

    Add files via upload

---
+++ ideas-service/IUserNameRegisterService.java
@@ -0,0 +1,19 @@
+package com.chenhao.service;
+
+import com.chenhao.dto.request.RegisterRequestDTO;
+import com.chenhao.dto.response.RegisterResponseDTO;
+
+/**
+ * @description:
+ * @author: chenhao
+ * @date: 2021-5-19 13:28
+ */
+public interface IUserNameRegisterService {
+    /**
+     * 通过用户名注册
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    RegisterResponseDTO registerByUserName(RegisterRequestDTO request) throws Exception;
+}
